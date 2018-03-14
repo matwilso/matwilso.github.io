226422f855c1b21a0cdf4636e6a1aae8d92121e6
matwilso <mattwilsonmbw@gmail.com>
2018-03-14 11:30:03 -0600

    dash update

--- _pages/dash_src/dash.js
+++ _pages/dash_src/dash.js
@@ -1,3 +1,68 @@
+// requires jquery, js-cookie
+
+var buttonDisp = {}
+buttonDisp['school'] = ['SCHOOL IS OUT', 'SCHOOL IS IN'];
+buttonDisp['workhours'] = ['FREE TIME', 'WORKING HOURS'];
+
+function setButtonText(idTag) {
+	if (JSON.parse(Cookies.get(idTag)) == false) {
+		$('#'+idTag).removeClass('red');
+		$('#'+idTag).addClass('green');
+		$('#'+idTag).text(buttonDisp[idTag][0]);
+	}
+	else {
+		$('#'+idTag).removeClass('green');
+		$('#'+idTag).addClass('red');
+		$('#'+idTag).text(buttonDisp[idTag][1]);
+	}
+}
+
+function checkButtonTimes() {
+	var today = new Date();
+	var dayOfWeek = today.getDay();
+	var timeOfDay = today.getHours()
+
+	if (dayOfWeek == 1) {
+		// set school on mondays
+		Cookies.set('school', true);
+	}
+	else {
+		// else only set school if it doesn't have a previous value
+		if (Cookies.get('school') == undefined || Cookies.get('school') == '') {
+			Cookies.set('school', true);
+		}
+	}
+	if (Cookies.get('workhours') != undefined || Cookies.get('workhours') == '') {
+		return;
+	}
+	// set work hours on based on time and day
+	if ((dayOfWeek != 0 && dayOfWeek != 6) && timeOfDay <= 18) {
+		Cookies.set('workhours', true);
+	}
+	else {
+		Cookies.set('workhours', false);
+	}
+}
+
+$(document).ready(function() {
+	checkButtonTimes();
+	setButtonText('school');
+	setButtonText('workhours');
+
+	$('#school').click(function() {
+		Cookies.set('school', !JSON.parse(Cookies.get('school')), { expires: 1 });
+		setButtonText('school');
+	});
+
+	$('#workhours').click(function() {
+		Cookies.set('workhours', !JSON.parse(Cookies.get('workhours')), { expires: 1 });
+		setButtonText('workhours');
+	});
+});
+
+
+// old shit
+/*
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
@@ -20,3 +85,4 @@ jQuery(function ($) {
         display = $('#time');
     startTimer(ninetyMinutes, display);
 });
+*/
